0858bddddf9386f7afcff8caaec699b9e3c9c764
Alexis <araykhel@cognitivescale.com>
2016-08-09 22:48:16 -0500

    Cleaning up a couple things.

--- src/main/scala/goodreads/Main.scala
+++ src/main/scala/goodreads/Main.scala
@@ -2,7 +2,7 @@ package goodreads
 
 object Main extends App{
 
-  val a = Author.getAuthorDeets(18541, "xQXvrwOTLq7xonOLcjt2A").getOrElse("There was an error. :(")
+  val a = Author.getAuthorDeets(18541, "k").getOrElse("There was an error. :(")
   println(a)
 
 }
